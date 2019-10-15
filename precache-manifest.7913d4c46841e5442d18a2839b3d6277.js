df88ede1dd4c987c3d0691d8d3fd882907ef34b5
ganesh <ganeshdalei1992@gmail.com>
2019-10-15 16:04:37 +0530

    Updates

---
+++ precache-manifest.7913d4c46841e5442d18a2839b3d6277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc583b1a579cbd84aa10a94207f203f",
+    "url": "/GaneshDalei/IDFC/index.html"
+  },
+  {
+    "revision": "a0c633b21a47eaf55472",
+    "url": "/GaneshDalei/IDFC/static/css/main.0627d965.chunk.css"
+  },
+  {
+    "revision": "74b39de41b1aced63c33",
+    "url": "/GaneshDalei/IDFC/static/js/2.75e47e9f.chunk.js"
+  },
+  {
+    "revision": "a0c633b21a47eaf55472",
+    "url": "/GaneshDalei/IDFC/static/js/main.91356a72.chunk.js"
+  },
+  {
+    "revision": "b1769884fc40c84fd117",
+    "url": "/GaneshDalei/IDFC/static/js/runtime-main.c2cc452c.js"
+  },
+  {
+    "revision": "6f24732a8fc41b4c13fcfcf38619c473",
+    "url": "/GaneshDalei/IDFC/static/media/visa-img.6f24732a.png"
+  },
+  {
+    "revision": "92de4110749773a6f850ec747fe5508d",
+    "url": "/GaneshDalei/IDFC/static/media/visa-img_lock.92de4110.png"
+  }
+]);
